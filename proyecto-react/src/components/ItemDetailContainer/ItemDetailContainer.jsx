f87bfa99f475e5b3fdf768521891c835f2a084e1
Matias <canda.matias2002@gmail.com>
2024-07-02 17:02:54 -0300

    commit

--- proyecto-react/src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ proyecto-react/src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getProductById } from "../../data/asyncMock";
-import { Box, Flex } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import ItemDetail from "../ItemDetail/ItemDetail";
 import { BarLoader } from "react-spinners";
 
