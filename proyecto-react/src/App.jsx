ba1351a1f940865c1d00f1bcf9a982470c862e53
Matias <canda.matias2002@gmail.com>
2024-07-03 22:40:34 -0300

    commit

--- proyecto-react/src/App.jsx
+++ proyecto-react/src/App.jsx
@@ -2,9 +2,9 @@ import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import PageNotFound from "./components/PageNotFound/PageNotFound";
 
 function App() {
@@ -13,10 +13,19 @@ function App() {
       <BrowserRouter>
         <NavBar />
         <Routes>
-          <Route path='/' element={<ItemListContainer title={'Sports Cars Scale Models'}/>} />
-          <Route path='/categorias/:categoryId' element={<ItemListContainer title={'Sports Cars Scale Models'}/>} />
-          <Route path='/producto/:productId' element={<ItemDetailContainer title={'Sports Cars Scale Models'}/>}/>
-          <Route path='*' element={<PageNotFound/>}></Route>
+          <Route
+            path="/"
+            element={<ItemListContainer title={"Sports Cars Scale Models"} />}
+          />
+          <Route
+            path="/categorias/:categoryId"
+            element={<ItemListContainer title={"Sports Cars Scale Models"} />}
+          />
+          <Route
+            path="/producto/:productId"
+            element={<ItemDetailContainer title={"Sports Cars Scale Models"} />}
+          />
+          <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
