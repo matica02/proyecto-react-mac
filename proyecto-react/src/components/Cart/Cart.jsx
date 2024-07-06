818a124eea0fe1a8e9bd702b9f8f4e0517900ff4
Matias <canda.matias2002@gmail.com>
2024-07-06 18:24:24 -0300

    commit

--- proyecto-react/src/components/Cart/Cart.jsx
+++ proyecto-react/src/components/Cart/Cart.jsx
@@ -1,48 +1,80 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
 import {
-    Table,
-    Thead,
-    Tbody,
-    Tfoot,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    TableContainer,
-    Button,
-  } from '@chakra-ui/react'
-import Context from '../../context/CartContext'
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Button,
+  Flex,
+  Text,
+  Link as ChakraLink,
+} from "@chakra-ui/react";
+import Context from "../../context/CartContext";
 import { IoIosRemoveCircle } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
-    const {cart , getTotalPrice , removeItem , clearCart} = useContext(Context)
-  return (
-    <TableContainer>
-        <Table variant='striped' colorScheme='teal'>
-            <Thead>
-                <Tr>
-                    <Th>Product</Th>
-                    <Th>Quantity</Th>
-                    <Th>Precio</Th>
-                    <Th>Subtotal</Th>
-                    <Th></Th>
-                </Tr>
-            </Thead>
-                <Tbody>
-                    {
-                        cart.map((prod) => (
-                            <Tr>
-                                <Td>{prod.nombre}</Td>
-                                <Td>{prod.quantity}</Td>
-                                <Td>{prod.precio}</Td>
-                                <Td>{prod.precio * prod.quantity}</Td>
-                                <Td>{<Button onClick={() => removeItem(prod.id)}><IoIosRemoveCircle /></Button>}</Td>
-                            </Tr>
-                    ))}    
-            </Tbody>
-        </Table>
-    </TableContainer>
-  )
-}
+  const { cart, getTotalPrice, removeItem, clearCart } = useContext(Context);
 
-export default Cart
+  /* falta el if para no mostrar el carrito cuando (lenght === 0) */
+
+  return (
+    <TableContainer
+      fontFamily={'"Oswald", sans-serif'}
+      fontSize={"25px"}
+      padding={"60px 50px 0px 60px"}
+    >
+      <Table variant="striped" colorScheme="pink">
+        <Thead>
+          <Tr>
+            <Th>Product</Th>
+            <Th>Quantity</Th>
+            <Th>Price</Th>
+            <Th>Subtotal</Th>
+            <Th></Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {cart.map((prod) => (
+            <Tr key={prod.id}>
+              <Td>{prod.nombre}</Td>
+              <Td>{prod.quantity}</Td>
+              <Td>${prod.precio}</Td>
+              <Td>${prod.precio * prod.quantity}</Td>
+              <Td>
+                {
+                  <Button
+                    onClick={() => removeItem(prod.id)}
+                    _hover={{ backgroundColor: "transparent" }}
+                    backgroundColor={"transparent"}
+                  >
+                    <IoIosRemoveCircle fontSize={"40px"} />
+                  </Button>
+                }
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+      <Flex justify={"space-between"} margin={"30px 60px 0px 0px"}>
+        <Button
+          border={"1px"}
+          _hover={{ backgroundColor: "red" }}
+          backgroundColor={"transparent"}
+          onClick={() => clearCart()}
+        >
+          Clear Cart
+        </Button>
+        <Text>Total: ${getTotalPrice()}</Text>
+        <Text textDecoration={"underline"} color={"green"}>
+          Proceed to payment
+        </Text>
+      </Flex>
+    </TableContainer>
+  );
+};
+
+export default Cart;
--- proyecto-react/src/components/ItemDetail/ItemDetail.jsx
+++ proyecto-react/src/components/ItemDetail/ItemDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import {
   Card,
   CardBody,
@@ -6,16 +6,18 @@ import {
   Stack,
   Heading,
   Text,
-  Button,
-  ButtonGroup,
   Divider,
   Image,
+  Link as ChakraLink,
+  Flex
 } from "@chakra-ui/react";
 import ItemCount from "../ItemCount/ItemCount";
 import { ToastContainer, toast } from "react-toastify";
 import Context from "../../context/CartContext";
+import { Link } from "react-router-dom";
 
 const ItemDetail = ({ id, nombre, img, descripcion, precio, stock }) => {
+  const [quantity, setQuantity] = useState(0)
   const {addItem} = useContext(Context)
   const onAdd = (quantity) => {
     const item = {
@@ -23,12 +25,13 @@ const ItemDetail = ({ id, nombre, img, descripcion, precio, stock }) => {
       nombre,
       precio
     }
+    setQuantity(quantity)
     addItem(item , quantity)
       toast(`Agregaste ${quantity} productos`);
   };
 
   return (
-    <Card maxW="xlg" backgroundColor="transparent" border="1px" margin={"50px"}>
+    <Card fontFamily={'"Oswald", sans-serif'} maxW="xlg" backgroundColor={"transparent"} border="1px" margin={"50px"}>
       <CardBody>
         <Image
           src={img}
@@ -41,17 +44,25 @@ const ItemDetail = ({ id, nombre, img, descripcion, precio, stock }) => {
           <Heading size="lg" fontFamily='"Oswald", sans-serif'>
             {nombre}
           </Heading>
-          <Text color="black" fontSize="35px" fontFamily='"Oswald", sans-serif'>
+          <Text color="black" fontSize="35px">
             ${precio}
           </Text>
-          <Text color="black" fontSize="25px" fontFamily='"Oswald", sans-serif'>
+          <Text color="black" fontSize="25px">
             {descripcion}
           </Text>
         </Stack>
       </CardBody>
       <Divider />
       <CardFooter>
-        <ItemCount initialValue={1} stock={stock} onAdd={onAdd} />
+        {
+          quantity === 0 ?
+          <ItemCount initialValue={1} stock={stock} onAdd={onAdd} />
+          :
+          <Flex textDecoration={'underline'} fontSize={'30px'}>
+            <ChakraLink as={Link} to='/cart'>Checkout</ChakraLink>
+            <ChakraLink as={Link} to='/cart' marginLeft={'30px'}>Continue shopping</ChakraLink>
+          </Flex>  
+        }
       </CardFooter>
       <ToastContainer />
     </Card>
